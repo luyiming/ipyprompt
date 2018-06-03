7cb180bd1e973763c574907d52d472ac80c184d8
luyiming <luyimingchn@gmail.com>
2018-06-04 00:35:49 +0800

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+## Usage:
+
+```
+pip install ipyprompt
+```
+
+Then in a new ipython shell, simply run:
+
+```
+%load_ext ipyprompt
+```
+
+## Screenshot
+![Screenshot](screenshots/screenshot.png)
+
+- Green / Red prompt before In / Out Cell: **current time**
+- Blue prompt after output: **execution time**
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+setuptools.setup(
   name = 'ipyprompt',
-  packages = ['ipyprompt'],
-  version = '0.1',
+  version = '0.0.1',
+  packages=setuptools.find_packages(),
   description = 'Custom IPython prompt with execution time included',
   author = 'luyiming',
   author_email = 'luyimingchn@gmail.com',
   url = 'https://github.com/luyiming/ipyprompt',
-  download_url = 'https://github.com/luyiming/ipyprompt/archive/0.1.tar.gz',
   keywords = ['ipython', 'jupyter', 'extension'],
   classifiers = [],
 )
